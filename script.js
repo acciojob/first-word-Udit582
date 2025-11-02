15ad3822199bb99013caba46a6c4f6cd3ada4ce6
Udit582 <uditpaliwal582@gmail.com>
2025-11-02 11:51:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 function firstWord(s) {
-  // your code here
+    // Trim leading/trailing spaces
+  s = s.trim();
+
+  // If string is empty after trimming, return empty string
+  if (s.length === 0) return "";
+
+  // Find index of first space
+  let spaceIndex = s.indexOf(" ");
+
+  // If no space found, return the whole string
+  if (spaceIndex === -1) return s;
+
+  // Otherwise, return substring up to the first space
+  return s.substring(0, spaceIndex);
+	
 }
 
-// Do not change the code below
+// Do not change the code beow
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
